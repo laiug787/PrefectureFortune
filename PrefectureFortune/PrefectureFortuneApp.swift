d25cd00497c82bcecb24e1815ec7632341654fc4
SegawaYusho <laiug787@icloud.com>
2023-06-26 01:22:36 +0900

    Initial Commit

---
+++ PrefectureFortune/PrefectureFortuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefectureFortuneApp.swift
+//  PrefectureFortune
+//
+//  Created by Yusho Segawa on 2023/06/26
+//
+
+import SwiftUI
+
+@main
+struct PrefectureFortuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
