b198aba23f4ded0e9456c73941db8fcf865c1b03
SegawaYusho <laiug787@icloud.com>
2023-07-01 15:11:58 +0900

    Update: PrefectureDetailView
    MapKitの追加

--- PrefectureFortune/Views/PrefecturesView/PrefectureDetailView.swift
+++ PrefectureFortune/Views/PrefecturesView/PrefectureDetailView.swift
@@ -6,39 +6,111 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct PrefectureDetailView: View {
     var prefecture: Prefecture
+    @State private var region = MKCoordinateRegion()
+    @State private var isExpanded: Bool = false
     
     var body: some View {
-        List {
-            Section("Prefecture") {
-                HStack(spacing: 20) {
-                    AsyncImage(url: URL(string: prefecture.logoUrl)) { image in
-                        image
+        ZStack {
+            Map(coordinateRegion: $region)
+                .edgesIgnoringSafeArea(.vertical)
+            VStack {
+                HStack {
+                    Spacer()
+                    Button {
+                        print("star")
+                    } label: {
+                        Image(systemName: "star.fill")
                             .resizable()
-                            .scaledToFit()
-                            .shadow(radius: 5)
-                            .frame(width: 100, height: 100)
-                    } placeholder: {
-                        ProgressView()
-                            .frame(width: 100, height: 100)
+                            .frame(width: 30.0, height: 30.0)
+                            .padding()
                     }
-                    VStack(alignment: .leading) {
-                        Text(prefecture.name)
-                            .font(.title)
-                        Text(prefecture.capital)
+                    .tint(.yellow)
+                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
+                    .compositingGroup()
+                    .shadow(radius: 4)
+                    .padding()
+                }
+                Spacer()
+                VStack {
+                    PrefectureListItem(prefecture)
+                    if isExpanded {
+                        Text(prefecture.brief)
+                    } else {
+                        Text("Show more")
                             .font(.subheadline)
                             .foregroundColor(.secondary)
                     }
                 }
-                LabeledContent("Citizen Day", value: prefecture.citizenDay?.date.monthDayString ?? "nil")
-                LabeledContent("Has coast line", value: prefecture.hasCoastLine.description.capitalized)
-                Text(prefecture.brief)
+                .padding(16)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
+                .frame(minWidth: 90, maxWidth: 350)
+                .padding(16)
+                .compositingGroup()
+                .shadow(radius: 4)
+                .onTapGesture {
+                    withAnimation {
+                        isExpanded.toggle()
+                    }
+                }
             }
         }
-        .navigationTitle("Detail")
         .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            let address = prefecture.capital
+            CLGeocoder().geocodeAddressString(address) { placemarks, error in
+                guard let latitude = placemarks?.first?.location?.coordinate.latitude else { return }
+                guard let longitude = placemarks?.first?.location?.coordinate.longitude else { return }
+                region = MKCoordinateRegion(
+                    center : CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
+                    latitudinalMeters: 5000.0,
+                    longitudinalMeters: 5000.0
+                )
+            }
+        }
+    }
+}
+
+struct PrefectureListItem: View {
+    var prefecture: Prefecture
+    
+    init(_ prefecture: Prefecture) {
+        self.prefecture = prefecture
+    }
+    
+    var body: some View {
+        HStack(spacing: 32) {
+            AsyncImage(url: URL(string: prefecture.logoUrl)) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 70, height: 70)
+            
+            VStack(alignment: .leading) {
+                Text(prefecture.name)
+                    .font(.title)
+                Text(prefecture.capital)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            .frame(minWidth: 120, maxWidth: 200, alignment: .leading)
+            
+            VStack(spacing: 16) {
+                if let citizenDay = prefecture.citizenDay {
+                    Label(citizenDay.date.monthDayString, systemImage: "calendar")
+                }
+                if prefecture.hasCoastLine {
+                    Image(systemName: "water.waves")
+                }
+            }
+            .foregroundColor(.secondary)
+        }
     }
 }
 
