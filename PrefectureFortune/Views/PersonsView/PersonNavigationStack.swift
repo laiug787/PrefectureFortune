f46fb83d26021378eb9f839ac40d4ed49fdbc7bf
SegawaYusho <laiug787@icloud.com>
2023-07-02 01:26:23 +0900

    Update: personListItem

--- PrefectureFortune/Views/PersonsView/PersonNavigationStack.swift
+++ PrefectureFortune/Views/PersonsView/PersonNavigationStack.swift
@@ -30,8 +30,9 @@ struct PersonNavigationStack: View {
                 List {
                     switch viewStyle {
                     case .users:
-                        ForEach(userVM.persons) { user in
-                            Text(user.name)
+                        ForEach(userVM.persons) { person in
+                            personListItem(person)
+                                .padding(.horizontal)
                         }
                     case .prefectures:
                         ForEach(userVM.prefectures) { prefecture in
@@ -39,8 +40,27 @@ struct PersonNavigationStack: View {
                         }
                     }
                 }
+                .listStyle(.plain)
             }
-            .navigationTitle("Favorites")
+            .navigationTitle("Collections")
+        }
+    }
+    
+    private func personListItem(_ person: PersonEntity) -> some View {
+        LabeledContent {
+            Grid(verticalSpacing: 4) {
+                GridRow {
+                    Image(systemName: "drop")
+                    Text(person.bloodType)
+                }
+                GridRow {
+                    Image(systemName: "birthday.cake")
+                    Text(person.birthday.monthDayString)
+                }
+            }
+        } label: {            
+            Label(person.name, systemImage: "person")
+                .font(.title2)
         }
     }
 }
