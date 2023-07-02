4369ac0ca8734ee08ef3981a6725a09747c719ca
SegawaYusho <laiug787@icloud.com>
2023-07-02 16:15:25 +0900

    Fix: NavigationStack -> NavigationView
    挙動がおかしいため

--- PrefectureFortune/Views/PredictView/UserRequestView.swift
+++ PrefectureFortune/Views/PredictView/UserRequestView.swift
@@ -18,7 +18,7 @@ struct UserRequestView: View {
     }
     
     var body: some View {
-        NavigationStack {
+        NavigationView {
             List {
                 Section {
                     PredictRequestListItem(person: $predictVM.person) {
